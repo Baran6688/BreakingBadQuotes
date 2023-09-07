2b09036a78e937002ab3d6c39cc1d2f9eab69850
Baran <123073442+Baran6688@users.noreply.github.com>
2023-09-07 09:16:22 +0300

    Updating Home and About Pages!

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,13 +2,10 @@ function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis itaque fugit explicabo? Consectetur hic quibusdam, officia ex placeat dolorum labore ratione est aperiam? Cum eum similique possimus enim itaque suscipit?
-        Nostrum dolore in animi totam cum deleniti itaque molestiae fugiat veritatis rerum, dolor ex odit sed error sit repellat, ducimus quam, earum non illo saepe! Repellendus quam quaerat recusandae expedita!
-        Molestiae in a incidunt accusamus obcaecati, hic reiciendis sed deleniti quibusdam nulla officiis eligendi. Assumenda laboriosam necessitatibus perspiciatis voluptate, ad natus quasi enim cupiditate facilis, iure, consequatur rem dolores doloremque?
-        A porro vel omnis, aliquid hic ipsa quaerat praesentium reprehenderit dolorum nihil autem dolorem tenetur nesciunt? Numquam iste molestias natus alias in rerum iusto eveniet ipsam quas ipsa, repellat temporibus!
-        Praesentium, magni! Exercitationem suscipit amet tenetur recusandae hic eaque aliquam possimus nostrum, rem culpa? Repellendus id aliquam esse autem, velit nisi aut, nemo saepe, natus adipisci dolorem. A, quo ea.
-      </p>
+
+      <h3>this website is just for fun which made by a Breaking Bad fan.</h3>
+      <p>in this website you can generate quotes from the series and either save them in your favourites, you also can post about the series in the posts section, but First you should be Logged In.</p>
+
     </div>
   );
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,13 +11,9 @@ function Home() {
     <div>
       <h1>Home</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
-        animi? Hic ad voluptate distinctio natus nihil reprehenderit doloribus
-        cumque soluta accusamus harum, eius ipsa totam tenetur iste nam
-        reiciendis ea. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Nisi ex soluta unde voluptatum veniam architecto, qui, fugit debitis
-        iste consequuntur quas nostrum tenetur similique ut sunt saepe!
-        Nesciunt, fugit enim.
+  Breaking Bad is an American crime drama television series created and produced by Vince Gilligan for AMC. Set and filmed in Albuquerque, New Mexico, the series follows Walter White (Bryan Cranston), an underpaid, overqualified, and dispirited high-school chemistry teacher who is struggling with a recent diagnosis of stage-three lung cancer. White turns to a life of crime and partners with a former student, Jesse Pinkman (Aaron Paul), to produce and distribute methamphetamine to secure his family's financial future before he dies, while navigating the dangers of the criminal underworld. Breaking Bad premiered on AMC on January 20, 2008, and concluded on September 29, 2013, after five seasons consisting of 62 episodes.
+
+  Among the show's co-stars are Anna Gunn and RJ Mitte as Walter's wife Skyler and son Walter Jr., and Betsy Brandt and Dean Norris as Skyler's sister Marie Schrader and her husband Hank, a DEA agent. Others include Bob Odenkirk as Walter's and Jesse's lawyer Saul Goodman, Jonathan Banks as private investigator and fixer Mike Ehrmantraut, and Giancarlo Esposito as drug kingpin Gus Fring. The final season introduces Jesse Plemons as the criminally ambitious Todd Alquist, and Laura Fraser as Lydia Rodarte-Quayle, a cunning business executive
       </p>
 
     </div>
